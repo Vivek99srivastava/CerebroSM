19e5d6c87b0b39835b148fda8b48dd0658f58b44
Vivek Srivastava <vivek99srivastava@gmail.com>
2018-09-19 23:40:52 +0530

    t

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CerebroSMAppComponent } from './cerebromanager/cerebro-sm-app.component';
+
+//Routing routes
+
+const routes: Routes = [
+{ path: 'cerebromanager',loadChildren:'./cerebromanager/cerebromanager.module#CerebromanagerModule'},
+{ path: 'demo',loadChildren:'./demo/demo.module#DemoModule'},
+{ path: '**' , redirectTo:'cerebromanager' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    
+    RouterModule.forRoot(routes)
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/cerebromanager/components/feature-toolbar/feature-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-toolbar',
+  templateUrl: './feature-toolbar.component.html',
+  styleUrls: ['./feature-toolbar.component.css']
+})
+export class FeatureToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
