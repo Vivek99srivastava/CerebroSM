c90ce158f6ab62ac61c3c49462138d00a0f2ecd8
Vivek Kumar19 <VK00508054@TechMahindra.com>
2018-09-21 15:44:38 +0530

    Added feature toolbar

--- src/app/cerebromanager/cerebromanager.module.ts
+++ src/app/cerebromanager/cerebromanager.module.ts
@@ -4,7 +4,7 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContainerComponent } from './components/main-container/main-container.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { FeatureToolbarComponent } from './components/feature-toolbar/feature-toolbar.component';
-
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 //cerebroSm-components
 import { CerebroSMAppComponent } from './cerebro-sm-app.component';
 
@@ -29,3 +29,5 @@ const routes: Routes = [
   declarations: [CerebroSMAppComponent, ToolbarComponent, MainContainerComponent, SideNavComponent, FeatureToolbarComponent]
 })
 export class CerebromanagerModule { }
+bootstrap: [CerebromanagerModule]
+platformBrowserDynamic().bootstrapModule(CerebromanagerModule);
