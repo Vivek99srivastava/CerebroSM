3859dc82866cc91ee299fa31ebd095f06a417971
Vivek Srivastava <vivek99srivastava@gmail.com>
2018-10-05 09:52:32 +0530

    Update sprint-burndown.component.ts

--- src/app/cerebromanager/components/sprint-burndown/sprint-burndown.component.ts
+++ src/app/cerebromanager/components/sprint-burndown/sprint-burndown.component.ts
@@ -13,3 +13,56 @@ export class SprintBurndownComponent implements OnInit {
   }
 
 }
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { Chart } from 'chart.js';
+
+// @Component({
+//   selector: 'app-sprint-burndown',
+//   templateUrl: './sprint-burndown.component.html',
+//   styleUrls: ['./sprint-burndown.component.css']
+// })
+// export class SprintBurndownComponent implements OnInit {
+
+//   LineChart=[];
+//   constructor() { }
+
+//   ngOnInit() {
+//     this.LineChart = new Chart('lineChart',{ 
+//       type:'line',
+//       data:{
+//         labels:["jan","feb","mach","april","may"],
+//         datasets:[{
+//         label:'no of items',
+//         data:[9,5,4,3,6,5,2,11,24,54],
+//         fill:false,
+//         lineTension:0.2,
+//         borderColor:'red',
+//         borderWidth:1
+//       }]
+//     },
+//     options:{
+//       title:{
+//         text:"Line chart",
+//         display:true
+//           },
+//           scales:{
+//             yAxes:[{
+//               ticks:{
+//                 beginAtZero:true
+//               }
+//             }]
+//           }
+//     })
+
+
+
+
+//   }
+
+// }
+
